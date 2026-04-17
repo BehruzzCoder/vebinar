932717caf8cba355ff4f7e8d11ea321fbf4ec23e
Behruz <behruzzzcoder@gmail.com>
2026-04-17 23:41:53 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxLSH40bpmVMzbNfrw3OS7DcHYK1048WaX3vULkO4rEU8s--t0J782jUYbBXkULPS04/exec";
+
+// Lead saqlash
+app.post("/webinar", async (req, res) => {
+  try {
+    const { name, phone } = req.body;
+
+    await axios.post(GOOGLE_SCRIPT_URL, {
+      name,
+      phone
+    });
+
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "xatolik" });
+  }
+});
+
+// Telegram click tracking
+app.post("/telegram-click", async (req, res) => {
+  try {
+    await axios.post(GOOGLE_SCRIPT_URL, {
+      event: "telegram_click"
+    });
+
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "xatolik" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server 3000 portda ishlayapti 🚀");
+});
